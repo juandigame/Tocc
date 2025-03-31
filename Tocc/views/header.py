5eee3af855ef3403ea1887e89736a6dc5b268942
David9709 <d.martin@utp.edu.co>
2025-03-30 22:27:20 -0500

    header primera parte

---
+++ Tocc/views/header.py
@@ -0,0 +1,10 @@
+import reflex as rx
+from Tocc.styles.styles import Size, Spacing
+from Tocc.styles.colors import Color, TextColor
+
+def header() -> rx.Component:
+    return rx.vstack(
+        rx.hstack(
+            
+        )
+    )
